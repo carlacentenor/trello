429d01738a7dd6c1565ec32d5c5aca335b18d9c0
carlacentenor <carlacentenor@gmail.com>
2017-11-16 17:38:41 -0500

    actualizando app.js version 0.0.6

--- js/app.js
+++ js/app.js
@@ -16,10 +16,67 @@ window.addEventListener('load', function () {
         form.setAttribute('placeholder', 'Añadir una lista...');
         form.setAttribute('class', 'input-style');
         btnSave.setAttribute('class', 'btn-style');
+        btnSave.setAttribute('id','save-js');
         /*agregando texto al boton*/
         btnSave.textContent = 'Guardar';
         /*Borrar al div inicial */
         parentSection.removeChild(boxAddList);
+        /* Creando Evento del boton guardar      */
+        btnSave.addEventListener('click',function(){
+            var textList= form.value;
+            /** Creando el nuevo cuadro      */
+            var containerList = document.createElement('div');
+            var titleList = document.createElement('p');
+            var boxInput = document.createElement('p');
+            
+            /*Agregando el texto  */
+            titleList.textContent=textList;
+            titleList.setAttribute('class','title-style');
+            boxInput.textContent ='Añadir una tarea';
+            /**Agregando a HTML */
+            containerList.appendChild(titleList);
+            containerList.appendChild(boxInput);
+            parentSection.appendChild(containerList);
+            parentSection.insertBefore(containerList,container);
+            /**Agregando Atributos */
+            containerList.setAttribute('class','box-form-list');
+            boxInput.setAttribute('class','box-input-style')
+            form.value='';
+            boxInput.addEventListener('click',function(){
+                /*Creando nuevos elementos de lista*/
+                var textAreaList = document.createElement('textarea');
+                var btnInputList =document.createElement('button');
+                /**Agragando Atributos */
+                btnInputList.setAttribute('class','btn-style');
+                textAreaList.setAttribute('class','text-area-style');
+                btnInputList.textContent ='Añadir';
+                /**Agregando al HTML */
+                containerList.appendChild(textAreaList);
+                textAreaList.focus(); /* Dar foco al textarea*/ 
+                containerList.removeChild(boxInput);
+                containerList.appendChild(btnInputList);
+
+                btnInputList.addEventListener('click',function(){
+                    var listItem = document.createElement('div');
+                    listItem.setAttribute('class','item-list-style')
+                    listItem.textContent = textAreaList.value;
+                    containerList.insertBefore(listItem,textAreaList);
+                    textAreaList.focus()
+                    textAreaList.value='';
+                })
+                
+
+
+                
+
+
+            })
+
+
+
+
+        })
+
     })
 
 })
